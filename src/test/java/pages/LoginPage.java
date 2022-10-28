8728cdc12d686f7da5b5a0ee86a4dec721137db6
PragtiPaliwal <paliwalpragati42@gmail.com>
2022-10-28 22:14:43 +0530

    HotelAppCucumberDemo First commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		
+	}
+	@FindBy(id="username")
+	private WebElement uName;
+	
+	@FindAll({
+		@FindBy(id="password"),
+		@FindBy(name="password")
+	})
+	private WebElement pwd;
+	
+	@FindAll({
+		@FindBy(id="login"),
+		@FindBy(className="button"),
+		@FindBy(name="login_button")
+	})
+	private WebElement loginbtn;
+	
+	public void enteruserName(String user) {
+   	(uName).sendKeys(user);
+    }
+    
+    public void enterPassword(String pass) {
+   	 (pwd).sendKeys(pass);
+    }
+    
+    public void clickLoginButton() {
+   	 (loginbtn).click();
+    }
+}
